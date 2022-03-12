d6f81db3a48486e9800382046a261e8428aa256e
Asadbek <asadbekabdumajidov7@gmail.com>
2022-03-12 17:00:16 +0500

    splesh page finished

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:uzesp/screen/home/homepage.dart';
+import 'package:uzesp/screen/splesh%20screen/splesh_page.dart';
+import 'package:uzesp/screen/splesh%20screen/welcome_1.dart';
 
 class RouteGenerate {
   Route? generate(RouteSettings settings) {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(
+          builder: (context) => const SpleshPage(),
+        );case '/w1':
+        return MaterialPageRoute(
+          builder: (context) => const WelcomePage1(),
+        );
+      case '/home':
+        return MaterialPageRoute(
           builder: (context) => const MyHomePage(),
         );
-     
     }
     return null;
   }
